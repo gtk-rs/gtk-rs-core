3b2fd12ef2da2ede9572b519ec21ba4b3e9769b2
Guillaume Gomez <guillaume1.gomez@gmail.com>
2020-12-14 17:16:24 +0100

    Fix license situation

--- gtk3-macros/src/lib.rs
+++ gtk3-macros/src/lib.rs
@@ -1,3 +1,5 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
 mod composite_template_derive;
 mod util;
 
--- gtk3-macros/src/util.rs
+++ gtk3-macros/src/util.rs
@@ -1,3 +1,5 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
 use anyhow::{bail, Result};
 use itertools::Itertools;
 use proc_macro2::{Ident, Span};
