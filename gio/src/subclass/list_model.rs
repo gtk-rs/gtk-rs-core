4264cb825a94fe0c265c761e4e48a2d8db5d22e2
ranfdev <ranfdev@gmail.com>
2021-12-18 09:46:54 +0100

    gio: add ListModel::snapshot and impl IntoIterator for ListModel

--- gio/src/subclass/list_model.rs
+++ gio/src/subclass/list_model.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+use crate::prelude::*;
 use crate::ListModel;
 use glib::subclass::prelude::*;
 use glib::translate::*;
@@ -145,4 +146,26 @@ where
         );
     };
     item.to_glib_full()
+}
+
+impl ListModel {
+    // rustdoc-stripper-ignore-next
+    /// Get an immutable snapshot of the container inside the `ListModel`.
+    /// Any modification done to the returned container `Vec` will not be
+    /// reflected on the `ListModel`.
+    pub fn snapshot(&self) -> Vec<glib::Object> {
+        let mut res = Vec::with_capacity(self.n_items() as usize);
+        for i in 0..self.n_items() {
+            res.push(self.item(i).unwrap())
+        }
+        res
+    }
+}
+impl std::iter::IntoIterator for ListModel {
+    type Item = glib::Object;
+    type IntoIter = std::vec::IntoIter<glib::Object>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.snapshot().into_iter()
+    }
 }
