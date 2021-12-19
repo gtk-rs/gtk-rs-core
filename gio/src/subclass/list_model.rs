122ca14a2807a12e01fa7dbdf32656ccd161cfe5
ranfdev <ranfdev@gmail.com>
2021-12-19 11:32:22 +0100

    gio: remove unused prelude

--- gio/src/subclass/list_model.rs
+++ gio/src/subclass/list_model.rs
@@ -1,6 +1,5 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::prelude::*;
 use crate::ListModel;
 use glib::subclass::prelude::*;
 use glib::translate::*;
