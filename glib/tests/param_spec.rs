8a7e09fa8be9b44e68c8d503a747a5bf1e70f6de
ranfdev <ranfdev@gmail.com>
2022-01-26 22:41:30 +0100

    add license at the top

--- glib/tests/param_spec.rs
+++ glib/tests/param_spec.rs
@@ -1,7 +1,11 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
 #[test]
 fn generated_builder() {
     let spec = glib::ParamSpecCharBuilder::new()
         .name("custom-char")
+        .flags(glib::ParamFlags::READABLE)
         .build();
     assert_eq!(spec.nick(), "custom-char");
+    assert_eq!(spec.flags(), glib::ParamFlags::READABLE);
 }
