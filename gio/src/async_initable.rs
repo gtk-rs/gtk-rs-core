1a38695a24348a5db6030ac31a99f3f9e291d6c7
Jason Francis <jafrancis999@gmail.com>
2022-04-19 13:03:44 -0400

    gio: Use InitableError in AsyncInitable constructors

--- gio/src/async_initable.rs
+++ gio/src/async_initable.rs
@@ -1,8 +1,10 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+use crate::initable::InitableError;
 use crate::traits::AsyncInitableExt;
 use crate::AsyncInitable;
 use crate::Cancellable;
+use futures_util::future;
 use glib::object::IsA;
 use glib::object::IsClass;
 use glib::value::ToValue;
@@ -15,19 +17,24 @@ impl AsyncInitable {
     pub fn new_async<
         O: Sized + IsClass + IsA<Object> + IsA<AsyncInitable>,
         P: IsA<Cancellable>,
-        Q: FnOnce(Result<O, glib::Error>) + 'static,
+        Q: FnOnce(Result<O, InitableError>) + 'static,
     >(
         properties: &[(&str, &dyn ToValue)],
         io_priority: glib::Priority,
         cancellable: Option<&P>,
         callback: Q,
     ) {
-        let obj = Object::new::<O>(properties).unwrap();
+        let obj = match Object::new::<O>(properties) {
+            Ok(obj) => obj,
+            Err(e) => return callback(Err(e.into())),
+        };
         unsafe {
             obj.init_async(
                 io_priority,
                 cancellable,
-                glib::clone!(@strong obj => move |res| callback(res.map(|_| obj))),
+                glib::clone!(@strong obj => move |res| {
+                    callback(res.map(|_| obj).map_err(|e| e.into()));
+                }),
             );
         }
     }
@@ -36,15 +43,19 @@ impl AsyncInitable {
     pub fn new_future<O: Sized + IsClass + IsA<Object> + IsA<AsyncInitable>>(
         properties: &[(&str, &dyn ToValue)],
         io_priority: glib::Priority,
-    ) -> Pin<Box_<dyn std::future::Future<Output = Result<O, glib::Error>> + 'static>> {
+    ) -> Pin<Box_<dyn std::future::Future<Output = Result<O, InitableError>> + 'static>> {
+        let obj = match Object::new::<O>(properties) {
+            Ok(obj) => obj,
+            Err(e) => return Box::pin(future::ready(Err(e.into()))),
+        };
         Box_::pin(crate::GioFuture::new(
-            &Object::new::<O>(properties).unwrap(),
+            &obj,
             move |obj, cancellable, send| unsafe {
                 obj.init_async(
                     io_priority,
                     Some(cancellable),
                     glib::clone!(@strong obj => move |res| {
-                        send.resolve(res.map(|_| obj));
+                        send.resolve(res.map(|_| obj).map_err(|e| e.into()));
                     }),
                 );
             },
@@ -52,20 +63,30 @@ impl AsyncInitable {
     }
 
     #[doc(alias = "g_async_initable_new_async")]
-    pub fn with_type<P: IsA<Cancellable>, Q: FnOnce(Result<Object, glib::Error>) + 'static>(
+    pub fn with_type<P: IsA<Cancellable>, Q: FnOnce(Result<Object, InitableError>) + 'static>(
         type_: Type,
         properties: &[(&str, &dyn ToValue)],
         io_priority: glib::Priority,
         cancellable: Option<&P>,
         callback: Q,
     ) {
-        assert!(type_.is_a(AsyncInitable::static_type()));
-        let obj = Object::with_type(type_, properties).unwrap();
+        if !type_.is_a(AsyncInitable::static_type()) {
+            return callback(Err(InitableError::NewObjectFailed(glib::bool_error!(
+                "Type '{}' is not async initable",
+                type_
+            ))));
+        }
+        let obj = match Object::with_type(type_, properties) {
+            Ok(obj) => obj,
+            Err(e) => return callback(Err(e.into())),
+        };
         unsafe {
             obj.unsafe_cast_ref::<Self>().init_async(
                 io_priority,
                 cancellable,
-                glib::clone!(@strong obj => move |res| callback(res.map(|_| obj))),
+                glib::clone!(@strong obj => move |res| {
+                    callback(res.map(|_| obj).map_err(|e| e.into()));
+                }),
             )
         };
     }
@@ -75,16 +96,24 @@ impl AsyncInitable {
         type_: Type,
         properties: &[(&str, &dyn ToValue)],
         io_priority: glib::Priority,
-    ) -> Pin<Box_<dyn std::future::Future<Output = Result<Object, glib::Error>> + 'static>> {
-        assert!(type_.is_a(AsyncInitable::static_type()));
+    ) -> Pin<Box_<dyn std::future::Future<Output = Result<Object, InitableError>> + 'static>> {
+        if !type_.is_a(AsyncInitable::static_type()) {
+            return Box::pin(future::ready(Err(InitableError::NewObjectFailed(
+                glib::bool_error!("Type '{}' is not async initable", type_),
+            ))));
+        }
+        let obj = match Object::with_type(type_, properties) {
+            Ok(obj) => obj,
+            Err(e) => return Box::pin(future::ready(Err(e.into()))),
+        };
         Box_::pin(crate::GioFuture::new(
-            &Object::with_type(type_, properties).unwrap(),
+            &obj,
             move |obj, cancellable, send| unsafe {
                 obj.unsafe_cast_ref::<Self>().init_async(
                     io_priority,
                     Some(cancellable),
                     glib::clone!(@strong obj => move |res| {
-                        send.resolve(res.map(|_| obj));
+                        send.resolve(res.map(|_| obj).map_err(|e| e.into()));
                     }),
                 );
             },
