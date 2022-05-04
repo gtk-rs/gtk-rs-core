a8924803020b6054d3a64c93c9c6b0018c0169f9
Mițca Dumitru <dumitru0mitca@gmail.com>
2022-05-04 21:52:00 +0300

    gio: Add safety docs for Unix and Win32 streams

--- gio/src/unix_output_stream.rs
+++ gio/src/unix_output_stream.rs
@@ -12,6 +12,13 @@ use std::os::unix::io::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 use socket::{AsRawFd, FromRawFd, IntoRawFd, RawFd};
 
 impl UnixOutputStream {
+    // rustdoc-stripper-ignore-next
+    /// Creates a new [`Self`] that takes ownership of the passed in fd.
+    ///
+    /// # Safety
+    /// You must not close the fd unless you've previously called [`UnixOutputStreamExtManual::set_close_fd`]
+    /// on this stream. At which point you may only do so when all references to this stream have
+    /// been dropped.
     #[doc(alias = "g_unix_output_stream_new")]
     pub unsafe fn take_fd<T: IntoRawFd>(fd: T) -> UnixOutputStream {
         let fd = fd.into_raw_fd();
@@ -19,6 +26,11 @@ impl UnixOutputStream {
         OutputStream::from_glib_full(ffi::g_unix_output_stream_new(fd, close_fd)).unsafe_cast()
     }
 
+    // rustdoc-stripper-ignore-next
+    /// Creates a new [`Self`] that does not take ownership of the passed in fd.
+    ///
+    /// # Safety
+    /// You may only close the fd if all references to this stream have been dropped.
     #[doc(alias = "g_unix_output_stream_new")]
     pub unsafe fn with_fd<T: AsRawFd>(fd: T) -> UnixOutputStream {
         let fd = fd.as_raw_fd();
@@ -38,6 +50,12 @@ pub trait UnixOutputStreamExtManual: Sized {
     #[doc(alias = "get_fd")]
     fn fd<T: FromRawFd>(&self) -> T;
 
+    // rustdoc-stripper-ignore-next
+    /// Sets whether the fd of this stream will be closed when the stream is closed.
+    ///
+    /// # Safety
+    /// If you pass in `false` as the parameter, you may only close the fd if the all references
+    /// to the stream have been dropped. If you pass in `true`, you must never call close.
     #[doc(alias = "g_unix_output_stream_set_close_fd")]
     unsafe fn set_close_fd(&self, close_fd: bool);
 }
