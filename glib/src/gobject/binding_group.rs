c4a6672d8a8191795dd6253d56ce8625f68412f2
Maximiliano Sandoval R <msandova@protonmail.com>
2022-05-07 02:33:15 -0400

    binding_group: Add test

--- glib/src/gobject/binding_group.rs
+++ glib/src/gobject/binding_group.rs
@@ -251,3 +251,188 @@ impl<'a> BindingGroupBuilder<'a> {
         self.try_build().unwrap()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::prelude::*;
+    use crate::subclass::prelude::*;
+
+    #[test]
+    fn binding_without_source() {
+        let binding_group = crate::BindingGroup::new();
+
+        let source = TestObject::default();
+        let target = TestObject::default();
+
+        assert!(source.find_property("name").is_some());
+        binding_group.bind("name", &target, "name").build();
+
+        binding_group.set_source(Some(&source));
+
+        source.set_name("test_source_name");
+        assert_eq!(source.name(), target.name());
+
+        target.set_name("test_target_name");
+        assert_eq!(source.name(), target.name());
+    }
+
+    #[test]
+    fn binding_with_source() {
+        let binding_group = crate::BindingGroup::new();
+
+        let source = TestObject::default();
+        let target = TestObject::default();
+
+        binding_group.set_source(Some(&source));
+
+        binding_group.bind("name", &target, "name").build();
+
+        source.set_name("test_source_name");
+        assert_eq!(source.name(), target.name());
+    }
+
+    #[test]
+    fn binding_to_transform() {
+        let binding_group = crate::BindingGroup::new();
+
+        let source = TestObject::default();
+        let target = TestObject::default();
+
+        binding_group.set_source(Some(&source));
+        binding_group
+            .bind("name", &target, "name")
+            .flags(crate::BindingFlags::SYNC_CREATE)
+            .transform_to(|_binding, value| {
+                let prev_name = value.get::<String>().unwrap();
+                assert_eq!(prev_name, "Hello");
+
+                Some("World".to_value())
+            })
+            .build();
+
+        source.set_name("Hello");
+        assert_eq!(target.name(), "World");
+    }
+
+    #[test]
+    fn binding_from_transform() {
+        let binding_group = crate::BindingGroup::new();
+
+        let source = TestObject::default();
+        let target = TestObject::default();
+
+        binding_group.set_source(Some(&source));
+        binding_group
+            .bind("name", &target, "name")
+            .flags(crate::BindingFlags::SYNC_CREATE | crate::BindingFlags::BIDIRECTIONAL)
+            .transform_from(|_binding, _value| Some("World".to_value()))
+            .build();
+
+        target.set_name("Hello");
+        assert_eq!(source.name(), "World");
+    }
+
+    mod imp {
+        use super::*;
+
+        use once_cell::sync::Lazy;
+        use std::cell::RefCell;
+
+        use crate as glib;
+
+        #[derive(Debug, Default)]
+        pub struct TestObject {
+            pub name: RefCell<String>,
+            pub enabled: RefCell<bool>,
+        }
+
+        #[crate::object_subclass]
+        impl ObjectSubclass for TestObject {
+            const NAME: &'static str = "Test";
+            type Type = super::TestObject;
+        }
+
+        impl ObjectImpl for TestObject {
+            fn properties() -> &'static [crate::ParamSpec] {
+                static PROPERTIES: Lazy<Vec<crate::ParamSpec>> = Lazy::new(|| {
+                    vec![
+                        crate::ParamSpecString::new(
+                            "name",
+                            "name",
+                            "name",
+                            None,
+                            crate::ParamFlags::READWRITE | crate::ParamFlags::EXPLICIT_NOTIFY,
+                        ),
+                        crate::ParamSpecBoolean::new(
+                            "enabled",
+                            "enabled",
+                            "enabled",
+                            false,
+                            crate::ParamFlags::READWRITE | crate::ParamFlags::EXPLICIT_NOTIFY,
+                        ),
+                    ]
+                });
+                PROPERTIES.as_ref()
+            }
+
+            fn property(
+                &self,
+                obj: &Self::Type,
+                _id: usize,
+                pspec: &crate::ParamSpec,
+            ) -> crate::Value {
+                match pspec.name() {
+                    "name" => obj.name().to_value(),
+                    "enabled" => obj.enabled().to_value(),
+                    _ => unimplemented!(),
+                }
+            }
+
+            fn set_property(
+                &self,
+                obj: &Self::Type,
+                _id: usize,
+                value: &crate::Value,
+                pspec: &crate::ParamSpec,
+            ) {
+                match pspec.name() {
+                    "name" => obj.set_name(value.get().unwrap()),
+                    "enabled" => obj.set_enabled(value.get().unwrap()),
+                    _ => unimplemented!(),
+                };
+            }
+        }
+    }
+
+    crate::wrapper! {
+        pub struct TestObject(ObjectSubclass<imp::TestObject>);
+    }
+
+    impl Default for TestObject {
+        fn default() -> Self {
+            crate::Object::new(&[]).unwrap()
+        }
+    }
+
+    impl TestObject {
+        fn name(&self) -> String {
+            self.imp().name.borrow().clone()
+        }
+
+        fn set_name(&self, name: &str) {
+            if name != self.imp().name.replace(name.to_string()).as_str() {
+                self.notify("name");
+            }
+        }
+
+        fn enabled(&self) -> bool {
+            *self.imp().enabled.borrow()
+        }
+
+        fn set_enabled(&self, enabled: bool) {
+            if enabled != self.imp().enabled.replace(enabled) {
+                self.notify("enabled");
+            }
+        }
+    }
+}
