726f188e17abf5c5cf506dfee014dca6c8f07d48
Sebastian Dröge <sebastian@centricular.com>
2022-06-06 09:52:52 +0300

    Remove is_windows_utf8 usage
    Co-authored-by: jim4067 <jimmyimpulse2@gmail.com>

--- glib/src/utils.rs
+++ glib/src/utils.rs
@@ -1,12 +1,8 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use crate::error::BoolError;
-use crate::gstring::GString;
 use crate::translate::*;
-use crate::Error;
 use std::ffi::{OsStr, OsString};
 use std::mem;
-use std::path::{Path, PathBuf};
 use std::ptr;
 
 // rustdoc-stripper-ignore-next
@@ -37,49 +33,6 @@ pub fn set_prgname(name: Option<&str>) {
     unsafe { ffi::g_set_prgname(name.to_glib_none().0) }
 }
 
-#[doc(alias = "g_getenv")]
-pub fn getenv<K: AsRef<OsStr>>(variable_name: K) -> Option<OsString> {
-    #[cfg(not(windows))]
-    use ffi::g_getenv;
-    #[cfg(windows)]
-    use ffi::g_getenv_utf8 as g_getenv;
-
-    unsafe { from_glib_none(g_getenv(variable_name.as_ref().to_glib_none().0)) }
-}
-
-#[doc(alias = "g_setenv")]
-pub fn setenv<K: AsRef<OsStr>, V: AsRef<OsStr>>(
-    variable_name: K,
-    value: V,
-    overwrite: bool,
-) -> Result<(), BoolError> {
-    #[cfg(not(windows))]
-    use ffi::g_setenv;
-    #[cfg(windows)]
-    use ffi::g_setenv_utf8 as g_setenv;
-
-    unsafe {
-        result_from_gboolean!(
-            g_setenv(
-                variable_name.as_ref().to_glib_none().0,
-                value.as_ref().to_glib_none().0,
-                overwrite.into_glib(),
-            ),
-            "Failed to set environment variable"
-        )
-    }
-}
-
-#[doc(alias = "g_unsetenv")]
-pub fn unsetenv<K: AsRef<OsStr>>(variable_name: K) {
-    #[cfg(not(windows))]
-    use ffi::g_unsetenv;
-    #[cfg(windows)]
-    use ffi::g_unsetenv_utf8 as g_unsetenv;
-
-    unsafe { g_unsetenv(variable_name.as_ref().to_glib_none().0) }
-}
-
 #[doc(alias = "g_environ_getenv")]
 pub fn environ_getenv<K: AsRef<OsStr>>(envp: &[OsString], variable: K) -> Option<OsString> {
     unsafe {
@@ -90,123 +43,12 @@ pub fn environ_getenv<K: AsRef<OsStr>>(envp: &[OsString], variable: K) -> Option
     }
 }
 
-#[doc(alias = "g_get_user_name")]
-#[doc(alias = "get_user_name")]
-pub fn user_name() -> OsString {
-    #[cfg(not(all(windows, target_arch = "x86")))]
-    use ffi::g_get_user_name;
-    #[cfg(all(windows, target_arch = "x86"))]
-    use ffi::g_get_user_name_utf8 as g_get_user_name;
-
-    unsafe { from_glib_none(g_get_user_name()) }
-}
-
-#[doc(alias = "g_get_real_name")]
-#[doc(alias = "get_real_name")]
-pub fn real_name() -> OsString {
-    #[cfg(not(all(windows, target_arch = "x86")))]
-    use ffi::g_get_real_name;
-    #[cfg(all(windows, target_arch = "x86"))]
-    use ffi::g_get_real_name_utf8 as g_get_real_name;
-
-    unsafe { from_glib_none(g_get_real_name()) }
-}
-
-#[doc(alias = "g_get_current_dir")]
-#[doc(alias = "get_current_dir")]
-pub fn current_dir() -> Option<PathBuf> {
-    #[cfg(not(windows))]
-    use ffi::g_get_current_dir;
-    #[cfg(windows)]
-    use ffi::g_get_current_dir_utf8 as g_get_current_dir;
-
-    unsafe { from_glib_full(g_get_current_dir()) }
-}
-
-#[doc(alias = "g_filename_to_uri")]
-pub fn filename_to_uri<P: AsRef<Path>>(
-    filename: P,
-    hostname: Option<&str>,
-) -> Result<GString, Error> {
-    #[cfg(not(windows))]
-    use ffi::g_filename_to_uri;
-    #[cfg(windows)]
-    use ffi::g_filename_to_uri_utf8 as g_filename_to_uri;
-
-    let hostname = hostname.to_glib_none();
-    unsafe {
-        let mut error = std::ptr::null_mut();
-        let ret = g_filename_to_uri(filename.as_ref().to_glib_none().0, hostname.0, &mut error);
-        if error.is_null() {
-            Ok(from_glib_full(ret))
-        } else {
-            Err(from_glib_full(error))
-        }
-    }
-}
-
-#[doc(alias = "g_filename_from_uri")]
-pub fn filename_from_uri(uri: &str) -> Result<(std::path::PathBuf, Option<GString>), Error> {
-    #[cfg(not(windows))]
-    use ffi::g_filename_from_uri;
-    #[cfg(windows)]
-    use ffi::g_filename_from_uri_utf8 as g_filename_from_uri;
-
-    unsafe {
-        let mut hostname = ptr::null_mut();
-        let mut error = ptr::null_mut();
-        let ret = g_filename_from_uri(uri.to_glib_none().0, &mut hostname, &mut error);
-        if error.is_null() {
-            Ok((from_glib_full(ret), from_glib_full(hostname)))
-        } else {
-            Err(from_glib_full(error))
-        }
-    }
-}
-
-#[doc(alias = "g_find_program_in_path")]
-pub fn find_program_in_path<P: AsRef<Path>>(program: P) -> Option<PathBuf> {
-    #[cfg(not(all(windows, target_arch = "x86")))]
-    use ffi::g_find_program_in_path;
-    #[cfg(all(windows, target_arch = "x86"))]
-    use ffi::g_find_program_in_path_utf8 as g_find_program_in_path;
-
-    unsafe { from_glib_full(g_find_program_in_path(program.as_ref().to_glib_none().0)) }
-}
-
-#[doc(alias = "g_get_home_dir")]
-#[doc(alias = "get_home_dir")]
-pub fn home_dir() -> std::path::PathBuf {
-    #[cfg(not(all(windows, target_arch = "x86")))]
-    use ffi::g_get_home_dir;
-    #[cfg(all(windows, target_arch = "x86"))]
-    use ffi::g_get_home_dir_utf8 as g_get_home_dir;
-
-    unsafe { from_glib_none(g_get_home_dir()) }
-}
-
-#[doc(alias = "g_get_tmp_dir")]
-#[doc(alias = "get_tmp_dir")]
-pub fn tmp_dir() -> std::path::PathBuf {
-    #[cfg(not(all(windows, target_arch = "x86")))]
-    use ffi::g_get_tmp_dir;
-    #[cfg(all(windows, target_arch = "x86"))]
-    use ffi::g_get_tmp_dir_utf8 as g_get_tmp_dir;
-
-    unsafe { from_glib_none(g_get_tmp_dir()) }
-}
-
 #[doc(alias = "g_mkstemp")]
 pub fn mkstemp<P: AsRef<std::path::Path>>(tmpl: P) -> i32 {
-    #[cfg(not(windows))]
-    use ffi::g_mkstemp;
-    #[cfg(windows)]
-    use ffi::g_mkstemp_utf8 as g_mkstemp;
-
     unsafe {
         // NOTE: This modifies the string in place, which is fine here because
         // to_glib_none() will create a temporary, NUL-terminated copy of the string.
-        g_mkstemp(tmpl.as_ref().to_glib_none().0)
+        ffi::g_mkstemp(tmpl.as_ref().to_glib_none().0)
     }
 }
 
@@ -255,16 +97,11 @@ pub fn mkdtemp_full(tmpl: impl AsRef<std::path::Path>, mode: i32) -> Option<std:
 pub fn file_get_contents(
     filename: impl AsRef<std::path::Path>,
 ) -> Result<crate::Slice<u8>, crate::Error> {
-    #[cfg(not(windows))]
-    use ffi::g_file_get_contents;
-    #[cfg(windows)]
-    use ffi::g_file_get_contents_utf8 as g_file_get_contents;
-
     unsafe {
         let mut contents = ptr::null_mut();
         let mut length = mem::MaybeUninit::uninit();
         let mut error = ptr::null_mut();
-        let _ = g_file_get_contents(
+        let _ = ffi::g_file_get_contents(
             filename.as_ref().to_glib_none().0,
             &mut contents,
             length.as_mut_ptr(),
