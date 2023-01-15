063c4d4d69220fb68b1279a9e0a2d02ff5680ba4
Paolo Borelli <pborelli@gnome.org>
2023-01-15 11:16:53 +0100

    gio: use StrV for the simple proxy resolver API

--- gio/src/simple_proxy_resolver.rs
+++ gio/src/simple_proxy_resolver.rs
@@ -1,34 +1,38 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
-use glib::{prelude::*, translate::*};
+use glib::{prelude::*, translate::*, IntoStrV};
 
 use crate::{ProxyResolver, SimpleProxyResolver};
 
 impl SimpleProxyResolver {
     #[doc(alias = "g_simple_proxy_resolver_new")]
     #[allow(clippy::new_ret_no_self)]
-    pub fn new(default_proxy: Option<&str>, ignore_hosts: &[&str]) -> ProxyResolver {
+    pub fn new(default_proxy: Option<&str>, ignore_hosts: impl IntoStrV) -> ProxyResolver {
         unsafe {
-            from_glib_full(ffi::g_simple_proxy_resolver_new(
-                default_proxy.to_glib_none().0,
-                ignore_hosts.to_glib_none().0,
-            ))
+            ignore_hosts.run_with_strv(|ignore_hosts| {
+                from_glib_full(ffi::g_simple_proxy_resolver_new(
+                    default_proxy.to_glib_none().0,
+                    ignore_hosts.as_ptr() as *mut _,
+                ))
+            })
         }
     }
 }
 
 pub trait SimpleProxyResolverExtManual: 'static {
     #[doc(alias = "g_simple_proxy_resolver_set_ignore_hosts")]
-    fn set_ignore_hosts(&self, ignore_hosts: &[&str]);
+    fn set_ignore_hosts(&self, ignore_hosts: impl IntoStrV);
 }
 
 impl<O: IsA<SimpleProxyResolver>> SimpleProxyResolverExtManual for O {
-    fn set_ignore_hosts(&self, ignore_hosts: &[&str]) {
+    fn set_ignore_hosts(&self, ignore_hosts: impl IntoStrV) {
         unsafe {
-            ffi::g_simple_proxy_resolver_set_ignore_hosts(
-                self.as_ref().to_glib_none().0,
-                ignore_hosts.to_glib_none().0,
-            );
+            ignore_hosts.run_with_strv(|ignore_hosts| {
+                ffi::g_simple_proxy_resolver_set_ignore_hosts(
+                    self.as_ref().to_glib_none().0,
+                    ignore_hosts.as_ptr() as *mut _,
+                );
+            })
         }
     }
 }
