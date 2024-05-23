67936d8f5a723449e7d5cb425140bde86f22eca7
sdroege <sdroege@users.noreply.github.com>
2024-05-23 09:28:04 +0000

    deploy: 8339a2ea980226cb58b9866083fea9c415df1dfb

--- git/docs/search.desc/pango_cairo_sys/pango_cairo_sys-desc-0-.js
+++ git/docs/search.desc/pango_cairo_sys/pango_cairo_sys-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("pango_cairo_sys", 0, "Returns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.")
+searchState.loadedDescShard("pango_cairo_sys", 0, "No value.\nSome value of type <code>T</code>.\nReturns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.")
