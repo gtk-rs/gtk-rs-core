5881a514b23c6bf27240aecdd540fa1ceb236c48
Paolo Borelli <borellip@amazon.com>
2024-06-01 10:53:50 +0200

    gio: use a builder to register a DBus object
    Make DBusConnection::register_object return a RegistrationBuilder
    so that one can decide which closures to set.
    Also add an example to demonstrate how this is used.

---
+++ examples/gio_dbus_register_object/main.rs
@@ -0,0 +1,83 @@
+use gio::prelude::*;
+use std::sync::mpsc::{channel, Receiver, Sender};
+
+const EXAMPLE_XML: &str = r#"
+  <node>
+    <interface name='com.github.gtk_rs.examples.HelloWorld'>
+      <method name='Hello'>
+        <arg type='s' name='name' direction='in'/>
+        <arg type='s' name='greet' direction='out'/>
+      </method>
+    </interface>
+  </node>
+"#;
+
+fn on_startup(app: &gio::Application, tx: &Sender<gio::RegistrationId>) {
+    let connection = app.dbus_connection().expect("connection");
+
+    let example = gio::DBusNodeInfo::for_xml(EXAMPLE_XML)
+        .ok()
+        .and_then(|e| e.lookup_interface("com.github.gtk_rs.examples.HelloWorld"))
+        .expect("Example interface");
+
+    if let Ok(id) = connection
+        .register_object("/com/github/gtk_rs/examples/HelloWorld", &example)
+        .method_call(
+            glib::clone!(@strong app => move |_, _, _, _, method, params, invocation| {
+                match method {
+                    "Hello" => {
+                        if let Some((name,)) = <(String,)>::from_variant(&params) {
+                            let greet = format!("Hello {name}!");
+                            println!("{greet}");
+                            invocation.return_value(Some(&(greet,).to_variant()));
+                        } else {
+                            invocation.return_error(gio::IOErrorEnum::Failed, "Invalid parameters");
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+                app.quit();
+            }),
+        )
+        .build()
+    {
+        println!("Registered object");
+        tx.send(id).unwrap();
+    } else {
+        eprintln!("Could not register object");
+    }
+}
+
+fn on_shutdown(app: &gio::Application, rx: &Receiver<gio::RegistrationId>) {
+    let connection = app.dbus_connection().expect("connection");
+    if let Ok(registration_id) = rx.try_recv() {
+        if connection.unregister_object(registration_id).is_ok() {
+            println!("Unregistered object");
+        } else {
+            eprintln!("Could not unregister object");
+        }
+    }
+}
+
+fn main() -> glib::ExitCode {
+    let app = gio::Application::builder()
+        .application_id("com.github.gtk-rs.examples.RegisterDBusObject")
+        .build();
+    let _guard = app.hold();
+    let (tx, rx) = channel::<gio::RegistrationId>();
+
+    app.connect_startup(move |app| {
+        on_startup(app, &tx);
+    });
+
+    app.connect_activate(move |_| {
+        println!("Waiting for DBus Hello method to be called. Call the following command from another terminal:");
+        println!("dbus-send --print-reply --dest=com.github.gtk-rs.examples.RegisterDBusObject /com/github/gtk_rs/examples/HelloWorld com.github.gtk_rs.examples.HelloWorld.Hello string:YourName");
+    });
+
+    app.connect_shutdown(move |app| {
+        on_shutdown(app, &rx);
+    });
+
+    app.run()
+}
