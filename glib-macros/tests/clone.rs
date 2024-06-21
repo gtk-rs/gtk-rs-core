f6241bdf7354e0ca1f0ec38ed988494d52e2bc81
Sebastian Dröge <sebastian@centricular.com>
2024-06-21 09:48:02 +0300

    glib-macros: Fix clone tests to work with both the unstable syntax enabled and not

--- glib-macros/tests/clone.rs
+++ glib-macros/tests/clone.rs
@@ -353,7 +353,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a, @weak b => |x| {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_3.rs:1:86
+ --> test_old_3.rs:1:86
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a, @weak b => |x| {}); }
   |                                                                                      ^"#,
@@ -361,7 +361,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a, @weak b => || {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_4.rs:1:86
+ --> test_old_4.rs:1:86
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a, @weak b => || {}); }
   |                                                                                      ^"#,
@@ -369,7 +369,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a, @weak b => |x| println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_5.rs:1:86
+ --> test_old_5.rs:1:86
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a, @weak b => |x| println!("a")); }
   |                                                                                      ^"#,
@@ -377,7 +377,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a, @weak b => || println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_6.rs:1:86
+ --> test_old_6.rs:1:86
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a, @weak b => || println!("a")); }
   |                                                                                      ^"#,
@@ -385,7 +385,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a => |x| {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_7.rs:1:77
+ --> test_old_7.rs:1:77
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a => |x| {}); }
   |                                                                             ^"#,
@@ -393,7 +393,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a => || {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_8.rs:1:77
+ --> test_old_8.rs:1:77
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a => || {}); }
   |                                                                             ^"#,
@@ -401,7 +401,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a => |x| println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_9.rs:1:77
+ --> test_old_9.rs:1:77
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a => |x| println!("a")); }
   |                                                                             ^"#,
@@ -409,7 +409,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak a => || println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_10.rs:1:77
+ --> test_old_10.rs:1:77
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak a => || println!("a")); }
   |                                                                             ^"#,
@@ -417,7 +417,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong self => move |x| {})",
         r#"error: Can't use `self` as variable name. Try storing it in a temporary variable or rename it using `as`.
- --> test_11.rs:1:74
+ --> test_old_11.rs:1:74
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong self => move |x| {}); }
   |                                                                          ^^^^
@@ -426,7 +426,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong self.v => move |x| {})",
         r#"error: `self.v`: Field accesses are not allowed as is, you must rename it!
- --> test_12.rs:1:79
+ --> test_old_12.rs:1:79
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong self.v => move |x| {}); }
   |                                                                               ^
@@ -435,7 +435,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => @default-return false, || {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_13.rs:1:100
+ --> test_old_13.rs:1:100
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => @default-return false, || {}); }
   |                                                                                                    ^"#,
@@ -443,7 +443,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => @default-return false, || println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_14.rs:1:100
+ --> test_old_14.rs:1:100
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => @default-return false, || println!("a")); }
   |                                                                                                    ^"#,
@@ -451,7 +451,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => @default-return false, |bla| {})",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_15.rs:1:100
+ --> test_old_15.rs:1:100
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => @default-return false, |bla| {}); }
   |                                                                                                    ^
@@ -460,7 +460,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => @default-return false, |bla| println!(\"a\"))",
         r#"error: Closure needs to be "moved" so please add `move` before closure
- --> test_16.rs:1:100
+ --> test_old_16.rs:1:100
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => @default-return false, |bla| println!("a")); }
   |                                                                                                    ^
@@ -469,7 +469,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => default-return false, move || {})",
         r#"error: Missing `@` before `default-return`
- --> test_17.rs:1:77
+ --> test_old_17.rs:1:77
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => default-return false, move || {}); }
   |                                                                             ^^^^^^^
@@ -478,7 +478,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@weak v => @default-return false move || {})",
         r#"error: Expected `,` after `@default-return false`, found `move`
- --> test_18.rs:1:99
+ --> test_old_18.rs:1:99
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@weak v => @default-return false move || {}); }
   |                                                                                                   ^^^^
@@ -487,7 +487,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@yolo v => move || {})",
         r#"error: Unknown keyword `yolo`, only `weak`, `weak-allow-none`, `to-owned` and `strong` are allowed
- --> test_19.rs:1:67
+ --> test_old_19.rs:1:67
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@yolo v => move || {}); }
   |                                                                   ^^^^
@@ -496,7 +496,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(v => move || {})",
         r#"error: Unexpected ident `v`: you need to specify if this is a weak or a strong clone.
- --> test_20.rs:1:66
+ --> test_old_20.rs:1:66
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(v => move || {}); }
   |                                                                  ^
@@ -505,7 +505,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => {println!(\"foo\");})",
         r#"error: Missing `move` and closure declaration
- --> test_21.rs:1:79
+ --> test_old_21.rs:1:79
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => {println!("foo");}); }
   |                                                                               ^^^^^^^^^^^^^^^^^^
@@ -514,7 +514,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v, @default-return lol => move || {println!(\"foo\");})",
         r#"error: `@default-return` should be after `=>`
- --> test_22.rs:1:78
+ --> test_old_22.rs:1:78
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v, @default-return lol => move || {println!("foo");}); }
   |                                                                              ^^^^^^^
@@ -523,7 +523,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@default-return lol, @strong v => move || {println!(\"foo\");})",
         r#"error: `@default-return` should be after `=>`
- --> test_23.rs:1:67
+ --> test_old_23.rs:1:67
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@default-return lol, @strong v => move || {println!("foo");}); }
   |                                                                   ^^^^^^^
@@ -533,7 +533,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => async || {println!(\"foo\");})",
         r#"error: Expected `move` after `async`, found `|`
- --> test_24.rs:1:85
+ --> test_old_24.rs:1:85
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => async || {println!("foo");}); }
   |                                                                                     ^"#,
@@ -541,7 +541,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => async {println!(\"foo\");})",
         r#"error: Expected `move` after `async`, found `{`
- --> test_25.rs:1:85
+ --> test_old_25.rs:1:85
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => async {println!("foo");}); }
   |                                                                                     ^^^^^^^^^^^^^^^^^^
@@ -550,7 +550,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => move || async {println!(\"foo\");})",
         r#"error: Expected `move` after `async`, found `{`
- --> test_26.rs:1:93
+ --> test_old_26.rs:1:93
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => move || async {println!("foo");}); }
   |                                                                                             ^^^^^^^^^^^^^^^^^^
@@ -559,7 +559,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => move || async println!(\"foo\");)",
         r#"error: Expected `move` after `async`, found `println`
- --> test_27.rs:1:93
+ --> test_old_27.rs:1:93
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => move || async println!("foo");); }
   |                                                                                             ^^^^^^^
@@ -568,7 +568,7 @@ const OLD_TESTS: &[(&str, &str)] = &[
     (
         "clone!(@strong v => move || async move println!(\"foo\");)",
         r#"error: Expected block after `| async move`
- --> test_28.rs:1:98
+ --> test_old_28.rs:1:98
   |
 1 | fn main() { use glib::clone; let v = std::rc::Rc::new(1); clone!(@strong v => move || async move println!("foo");); }
   |                                                                                                  ^^^^^^^
@@ -590,11 +590,20 @@ fn clone_failures() {
     }
 
     for (index, (expr, err)) in OLD_TESTS.iter().enumerate() {
+        // If the new syntax is enabled some of the tests above go into the new code and produce a
+        // different error message
+        #[cfg(feature = "unstable-clone-syntax")]
+        {
+            if [0, 1, 2, 20].contains(&index) {
+                continue;
+            }
+        }
+
         let prefix = "fn main() { use glib::clone; let v = std::rc::Rc::new(1); ";
         let suffix = "; }";
         let output = format!("{prefix}{expr}{suffix}");
 
-        t.compile_fail_inline_check_sub(&format!("test_{index}"), &output, err);
+        t.compile_fail_inline_check_sub(&format!("test_old_{index}"), &output, err);
     }
 }
 
