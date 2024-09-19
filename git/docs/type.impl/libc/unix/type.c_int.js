5cac2562cd227aa469f529c85020c641576a80f9
sdroege <sdroege@users.noreply.github.com>
2024-09-19 11:36:00 +0000

    deploy: a1a0fdc1e02409a40fe706e18fea19abad6320fa

--- git/docs/type.impl/libc/unix/type.c_int.js
+++ git/docs/type.impl/libc/unix/type.c_int.js
@@ -1,9 +1,9 @@
 (function() {
-    var type_impls = Object.fromEntries([["cairo_sys",[]],["gdk_pixbuf_sys",[]],["gio_sys",[]],["glib_sys",[]],["graphene_sys",[]],["pango_sys",[]]]);
+    var type_impls = Object.fromEntries([["cairo_sys",[]],["gio_sys",[]]]);
     if (window.register_type_impls) {
         window.register_type_impls(type_impls);
     } else {
         window.pending_type_impls = type_impls;
     }
 })()
-//{"start":55,"fragment_lengths":[16,22,15,16,20,17]}
+//{"start":55,"fragment_lengths":[16,15]}
