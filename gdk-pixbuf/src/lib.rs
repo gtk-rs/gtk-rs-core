05c8ec5900d635dd0014b3f0c4e52a277733af0f
Sebastian Dröge <sebastian@centricular.com>
2024-11-29 17:23:59 +0200

    Fix / silence various new Rust 1.83 clippy warnings

--- gdk-pixbuf/src/lib.rs
+++ gdk-pixbuf/src/lib.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+#![allow(clippy::manual_c_str_literals)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
