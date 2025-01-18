aaf4d8ccbc5517d75ca88de101cd6378017f977a
Sebastian Wiesner <sebastian@swsnr.de>
2025-01-18 15:05:58 +0100

    Rewrite DBus registration example with inheritance
    This is more in line how application classes would be used in real world
    applications which usually create a derived application class which
    holds the overall state.
    
    And it prepares for testing the dbus_register vfunc subsequently.

--- examples/gio_dbus_register_object/main.rs
+++ examples/gio_dbus_register_object/main.rs
@@ -1,126 +1,162 @@
-use gio::{prelude::*, IOErrorEnum};
-use std::{
-    sync::mpsc::{channel, Receiver, Sender},
-    time::Duration,
-};
+use gio::prelude::*;
 
-const EXAMPLE_XML: &str = r#"
-  <node>
-    <interface name='com.github.gtk_rs.examples.HelloWorld'>
-      <method name='Hello'>
-        <arg type='s' name='name' direction='in'/>
-        <arg type='s' name='greet' direction='out'/>
-      </method>
-      <method name='SlowHello'>
-        <arg type='s' name='name' direction='in'/>
-        <arg type='u' name='delay' direction='in'/>
-        <arg type='s' name='greet' direction='out'/>
-      </method>
-    </interface>
-  </node>
+glib::wrapper! {
+    pub struct SampleApplication(ObjectSubclass<imp::SampleApplication>)
+        @extends gio::Application,
+        @implements gio::ActionGroup, gio::ActionMap;
+}
+
+impl Default for SampleApplication {
+    fn default() -> Self {
+        glib::Object::builder()
+            .property(
+                "application-id",
+                "com.github.gtk-rs.examples.RegisterDBusObject",
+            )
+            .build()
+    }
+}
+
+mod imp {
+    use std::cell::RefCell;
+    use std::time::Duration;
+
+    use gio::prelude::*;
+    use gio::subclass::prelude::*;
+    use gio::{DBusConnection, IOErrorEnum};
+
+    const EXAMPLE_XML: &str = r#"
+<node>
+  <interface name='com.github.gtk_rs.examples.HelloWorld'>
+    <method name='Hello'>
+      <arg type='s' name='name' direction='in'/>
+      <arg type='s' name='greet' direction='out'/>
+    </method>
+    <method name='SlowHello'>
+      <arg type='s' name='name' direction='in'/>
+      <arg type='u' name='delay' direction='in'/>
+      <arg type='s' name='greet' direction='out'/>
+    </method>
+  </interface>
+</node>
 "#;
 
-#[derive(Debug, glib::Variant)]
-struct Hello {
-    name: String,
-}
-
-#[derive(Debug, glib::Variant)]
-struct SlowHello {
-    name: String,
-    delay: u32,
-}
-
-#[derive(Debug)]
-enum HelloMethod {
-    Hello(Hello),
-    SlowHello(SlowHello),
-}
-
-impl DBusMethodCall for HelloMethod {
-    fn parse_call(
-        _obj_path: &str,
-        _interface: Option<&str>,
-        method: &str,
-        params: glib::Variant,
-    ) -> Result<Self, glib::Error> {
-        match method {
-            "Hello" => Ok(params.get::<Hello>().map(Self::Hello)),
-            "SlowHello" => Ok(params.get::<SlowHello>().map(Self::SlowHello)),
-            _ => Err(glib::Error::new(IOErrorEnum::Failed, "No such method")),
-        }
-        .and_then(|p| p.ok_or_else(|| glib::Error::new(IOErrorEnum::Failed, "Invalid parameters")))
+    #[derive(Debug, glib::Variant)]
+    struct Hello {
+        name: String,
     }
-}
 
-fn on_startup(app: &gio::Application, tx: &Sender<gio::RegistrationId>) {
-    let connection = app.dbus_connection().expect("connection");
+    #[derive(Debug, glib::Variant)]
+    struct SlowHello {
+        name: String,
+        delay: u32,
+    }
 
-    let example = gio::DBusNodeInfo::for_xml(EXAMPLE_XML)
-        .ok()
-        .and_then(|e| e.lookup_interface("com.github.gtk_rs.examples.HelloWorld"))
-        .expect("Example interface");
+    #[derive(Debug)]
+    enum HelloMethod {
+        Hello(Hello),
+        SlowHello(SlowHello),
+    }
 
-    if let Ok(id) = connection
-        .register_object("/com/github/gtk_rs/examples/HelloWorld", &example)
-        .typed_method_call::<HelloMethod>()
-        .invoke_and_return_future_local(|_, sender, call| {
-            println!("Method call from {sender:?}");
-            async {
-                match call {
-                    HelloMethod::Hello(Hello { name }) => {
-                        let greet = format!("Hello {name}!");
-                        println!("{greet}");
-                        Ok(Some(greet.to_variant()))
+    impl DBusMethodCall for HelloMethod {
+        fn parse_call(
+            _obj_path: &str,
+            _interface: Option<&str>,
+            method: &str,
+            params: glib::Variant,
+        ) -> Result<Self, glib::Error> {
+            match method {
+                "Hello" => Ok(params.get::<Hello>().map(Self::Hello)),
+                "SlowHello" => Ok(params.get::<SlowHello>().map(Self::SlowHello)),
+                _ => Err(glib::Error::new(IOErrorEnum::Failed, "No such method")),
+            }
+            .and_then(|p| {
+                p.ok_or_else(|| glib::Error::new(IOErrorEnum::Failed, "Invalid parameters"))
+            })
+        }
+    }
+
+    #[derive(Default)]
+    pub struct SampleApplication {
+        registration_id: RefCell<Option<gio::RegistrationId>>,
+    }
+
+    impl SampleApplication {
+        fn register_object(&self, connection: &DBusConnection) {
+            let example = gio::DBusNodeInfo::for_xml(EXAMPLE_XML)
+                .ok()
+                .and_then(|e| e.lookup_interface("com.github.gtk_rs.examples.HelloWorld"))
+                .expect("Example interface");
+
+            if let Ok(id) = connection
+                .register_object("/com/github/gtk_rs/examples/HelloWorld", &example)
+                .typed_method_call::<HelloMethod>()
+                .invoke_and_return_future_local(|_, sender, call| {
+                    println!("Method call from {sender:?}");
+                    async {
+                        match call {
+                            HelloMethod::Hello(Hello { name }) => {
+                                let greet = format!("Hello {name}!");
+                                println!("{greet}");
+                                Ok(Some(greet.to_variant()))
+                            }
+                            HelloMethod::SlowHello(SlowHello { name, delay }) => {
+                                glib::timeout_future(Duration::from_secs(delay as u64)).await;
+                                let greet = format!("Hello {name} after {delay} seconds!");
+                                println!("{greet}");
+                                Ok(Some(greet.to_variant()))
+                            }
+                        }
                     }
-                    HelloMethod::SlowHello(SlowHello { name, delay }) => {
-                        glib::timeout_future(Duration::from_secs(delay as u64)).await;
-                        let greet = format!("Hello {name} after {delay} seconds!");
-                        println!("{greet}");
-                        Ok(Some(greet.to_variant()))
-                    }
+                })
+                .build()
+            {
+                println!("Registered object");
+                self.registration_id.replace(Some(id));
+            } else {
+                eprintln!("Could not register object");
+            }
+        }
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for SampleApplication {
+        const NAME: &'static str = "SampleApplication";
+
+        type Type = super::SampleApplication;
+
+        type ParentType = gio::Application;
+    }
+
+    impl ObjectImpl for SampleApplication {}
+
+    impl ApplicationImpl for SampleApplication {
+        fn startup(&self) {
+            self.parent_startup();
+            let connection = self.obj().dbus_connection().expect("connection");
+            self.register_object(&connection);
+        }
+
+        fn shutdown(&self) {
+            if let Some(id) = self.registration_id.take() {
+                let connection = self.obj().dbus_connection().expect("connection");
+                if connection.unregister_object(id).is_ok() {
+                    println!("Unregistered object");
+                } else {
+                    eprintln!("Could not unregister object");
                 }
             }
-        })
-        .build()
-    {
-        println!("Registered object");
-        tx.send(id).unwrap();
-    } else {
-        eprintln!("Could not register object");
-    }
-}
+        }
 
-fn on_shutdown(app: &gio::Application, rx: &Receiver<gio::RegistrationId>) {
-    let connection = app.dbus_connection().expect("connection");
-    if let Ok(registration_id) = rx.try_recv() {
-        if connection.unregister_object(registration_id).is_ok() {
-            println!("Unregistered object");
-        } else {
-            eprintln!("Could not unregister object");
+        fn activate(&self) {
+            println!("Waiting for DBus Hello method to be called. Call the following command from another terminal:");
+            println!("dbus-send --print-reply --dest=com.github.gtk-rs.examples.RegisterDBusObject /com/github/gtk_rs/examples/HelloWorld com.github.gtk_rs.examples.HelloWorld.Hello string:YourName");
         }
     }
 }
 
 fn main() -> glib::ExitCode {
-    let app = gio::Application::builder()
-        .application_id("com.github.gtk-rs.examples.RegisterDBusObject")
-        .build();
+    let app = SampleApplication::default();
     let _guard = app.hold();
-    let (tx, rx) = channel::<gio::RegistrationId>();
-
-    app.connect_startup(move |app| {
-        on_startup(app, &tx);
-    });
-
-    app.connect_activate(move |_| {
-        println!("Waiting for DBus Hello method to be called. Call the following command from another terminal:");
-        println!("dbus-send --print-reply --dest=com.github.gtk-rs.examples.RegisterDBusObject /com/github/gtk_rs/examples/HelloWorld com.github.gtk_rs.examples.HelloWorld.Hello string:YourName");
-    });
-
-    app.connect_shutdown(move |app| {
-        on_shutdown(app, &rx);
-    });
-
     app.run()
 }
