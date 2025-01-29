e2731927bca314383ca2e68451f6713264c4064a
jgcodes2020 <j349guo@uwaterloo.ca>
2025-01-28 22:44:53 -0500

    add license headers to files that were missing one

--- glib-macros/src/signals_attribute.rs
+++ glib-macros/src/signals_attribute.rs
@@ -1,3 +1,5 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
 use bitflags::bitflags;
 use proc_macro2::{Literal, Span, TokenStream};
 use quote::ToTokens;
--- glib-macros/tests/signals.rs
+++ glib-macros/tests/signals.rs
@@ -1,3 +1,5 @@
+// Take a look at the license at the top of the repository in the LICENSE file.
+
 use std::{cell::Cell, rc::Rc};
 
 use glib::object::ObjectExt;
