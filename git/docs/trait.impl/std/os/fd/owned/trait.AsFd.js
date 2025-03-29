b849506ad00c8ba75af71defc30e4bec28fc52b0
sdroege <sdroege@users.noreply.github.com>
2025-03-29 15:47:39 +0000

    deploy: 54331d5204bd56296f2a972b92d95aa8acb9a852

---
+++ git/docs/trait.impl/std/os/fd/owned/trait.AsFd.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gio",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"gio/struct.Socket.html\" title=\"struct gio::Socket\">Socket</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"gio/struct.UnixInputStream.html\" title=\"struct gio::UnixInputStream\">UnixInputStream</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/os/fd/owned/trait.AsFd.html\" title=\"trait std::os::fd::owned::AsFd\">AsFd</a> for <a class=\"struct\" href=\"gio/struct.UnixOutputStream.html\" title=\"struct gio::UnixOutputStream\">UnixOutputStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[813]}
