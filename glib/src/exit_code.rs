e498edea19c326665449c6c0ec74bffa5862dc84
Philipp Jungkamp <p.jungkamp@gmx.net>
2025-03-31 12:20:01 +0200

    glib: Document and improve `ExitCode`
    This adds documentation for the `ExitCode` type, constants and functions.
    
    Converting a `glib::ExitCode` to a `std::process::ExitCode` will map all
    values that don't fit into `u8` to `u8::MAX`.
    
    The new `ExitCode::CONTINUE` constant can be used to indicate that default
    handling of `gio::Application` commandline handling should continue.

--- glib/src/exit_code.rs
+++ glib/src/exit_code.rs
@@ -2,13 +2,32 @@
 
 use std::process::Termination;
 
+// rustdoc-stripper-ignore-next
+/// Wrapper around an [`i32`] exit code that implements [`std::process::Termination`].
+///
+/// - a zero exit code means success.
+/// - a positive exit code means failure.
+/// - a negative exit code is reserved as special.
+///
+/// Values outside the range of [`u8`] will be mapped to [`u8::MAX`] when converted to [`std::process::ExitCode`].
 #[derive(Clone, Copy, PartialEq, Eq, Debug, PartialOrd, Ord)]
 pub struct ExitCode(i32);
 
 impl ExitCode {
-    pub const SUCCESS: Self = Self(0);
-    pub const FAILURE: Self = Self(1);
+    // rustdoc-stripper-ignore-next
+    /// The canonical ExitCode for successful termination on this platform.
+    pub const SUCCESS: Self = Self(libc::EXIT_SUCCESS);
 
+    // rustdoc-stripper-ignore-next
+    /// The canonical ExitCode for unsuccessful termination on this platform.
+    pub const FAILURE: Self = Self(libc::EXIT_FAILURE);
+
+    // rustdoc-stripper-ignore-next
+    /// A special negative value to indicate that default handling should continue.
+    pub const CONTINUE: Self = Self(-1);
+
+    // rustdoc-stripper-ignore-next
+    /// Returns the wrapped `i32` value.
     pub fn value(&self) -> i32 {
         self.0
     }
@@ -21,13 +40,19 @@ impl From<i32> for ExitCode {
 }
 
 impl From<ExitCode> for i32 {
-    fn from(value: ExitCode) -> Self {
-        value.0
+    fn from(code: ExitCode) -> Self {
+        code.value()
+    }
+}
+
+impl From<ExitCode> for std::process::ExitCode {
+    fn from(code: ExitCode) -> Self {
+        Self::from(u8::try_from(code.0).unwrap_or(u8::MAX))
     }
 }
 
 impl Termination for ExitCode {
     fn report(self) -> std::process::ExitCode {
-        std::process::ExitCode::from(self.0 as u8)
+        self.into()
     }
 }
