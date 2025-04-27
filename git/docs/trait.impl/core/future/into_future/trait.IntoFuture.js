967470a109d09643ccf337e3d74c3c5cbc68df77
sdroege <sdroege@users.noreply.github.com>
2025-04-27 09:47:48 +0000

    deploy: 10e4c61aa012bc7f69232223ef0d4dbf6bf7726e

---
+++ git/docs/trait.impl/core/future/into_future/trait.IntoFuture.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gio",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/into_future/trait.IntoFuture.html\" title=\"trait core::future::into_future::IntoFuture\">IntoFuture</a> for &amp;<a class=\"struct\" href=\"gio/struct.Cancellable.html\" title=\"struct gio::Cancellable\">Cancellable</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/future/into_future/trait.IntoFuture.html\" title=\"trait core::future::into_future::IntoFuture\">IntoFuture</a> for <a class=\"struct\" href=\"gio/struct.Cancellable.html\" title=\"struct gio::Cancellable\">Cancellable</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[608]}
