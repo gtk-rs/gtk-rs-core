52ed1018b6a13725fb1c90d65cce8f13b572651f
sdroege <sdroege@users.noreply.github.com>
2025-05-21 10:52:35 +0000

    deploy: 17a97faa4f7cd742d29b37bbf73785fd7ce23c3a

--- git/docs/graphene/sidebar-items.js
+++ git/docs/graphene/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["EulerOrder","RayIntersectionKind"],"mod":["prelude"],"struct":["Box","Euler","Frustum","Matrix","Plane","Point","Point3D","Quad","Quaternion","Ray","Rect","Size","Sphere","Triangle","Vec2","Vec3","Vec4"]};
+window.SIDEBAR_ITEMS = {"enum":["EulerOrder","RayIntersectionKind"],"mod":["prelude"],"struct":["Box","Box2D","Euler","Frustum","Matrix","Plane","Point","Point3D","Quad","Quaternion","Ray","Rect","Size","Sphere","Triangle","Vec2","Vec3","Vec4"]};
