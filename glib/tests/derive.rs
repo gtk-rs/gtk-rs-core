7b4ffff1879bc05f334f0d015302e65cf381e35d
Sebastian Dröge <sebastian@centricular.com>
2025-09-21 12:50:50 +0300

    glib: Fix some new 1.90 clippy warnings

--- glib/tests/derive.rs
+++ glib/tests/derive.rs
@@ -5,15 +5,18 @@ use glib::{Downgrade, Object};
 #[test]
 fn derive_downgrade() {
     #[derive(Downgrade)]
+    #[allow(dead_code)]
     pub struct NewType(Object);
 
     #[derive(Downgrade)]
+    #[allow(dead_code)]
     pub struct Struct {
         o1: Object,
         o2: std::rc::Rc<u32>,
     }
 
     #[derive(Downgrade)]
+    #[allow(dead_code)]
     pub enum Enum {
         None,
         Pair { x: Object, y: Object },
@@ -23,9 +26,11 @@ fn derive_downgrade() {
     }
 
     #[derive(Downgrade)]
+    #[allow(dead_code)]
     pub struct TypedWrapper<T>(Object, PhantomData<T>);
 
     #[derive(Downgrade)]
+    #[allow(dead_code)]
     pub enum TypedEnum<T> {
         This(Object, PhantomData<T>),
         That(Object, PhantomData<T>),
