gtk-rs/gtk-rs-core

commit 27e19a89abf126426a2353976b63420b76cc4603

win32: Guard std::os::windows usage
Otherwise we fail to find std::os::windows when building docs

glib-win32/src/functions.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 
 #[doc(alias = "g_win32_get_package_installation_directory_of_module")]
 #[doc(alias = "get_package_installation_directory_of_module")]
+#[cfg(all(docsrs, windows))]
 pub fn package_installation_directory_of_module(
     hmodule: std::os::windows::raw::HANDLE,
 ) -> Result<PathBuf, std::io::Error> {

gio-win32/src/input_stream.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+#[cfg(any(windows, docsrs))]
 use std::os::windows::io::{AsRawHandle, FromRawHandle, IntoRawHandle, RawHandle};
 
 use glib::{prelude::*, translate::*};

gio-win32/src/output_stream.rs
@@ -1,5 +1,6 @@
 // Take a look at the license at the top of the repository in the LICENSE file.
 
+#[cfg(any(windows, docsrs))]
 use std::os::windows::io::{AsRawHandle, FromRawHandle, IntoRawHandle, RawHandle};
 
 use glib::{prelude::*, translate::*};

commit 319879cc83d1280e82c4de6abb5b88f88fe20385

glib-win32: fix export of function
Regression from the commit: 27e19a89abf126426a2353976b63420b76cc4603
The function must be always exported for windows

glib-win32/src/functions.rs
@@ -9,7 +9,7 @@ use crate::ffi;
 
 #[doc(alias = "g_win32_get_package_installation_directory_of_module")]
 #[doc(alias = "get_package_installation_directory_of_module")]
-#[cfg(all(docsrs, windows))]
+#[cfg(any(windows, docsrs))]
 pub fn package_installation_directory_of_module(
     hmodule: std::os::windows::raw::HANDLE,
 ) -> Result<PathBuf, std::io::Error> {